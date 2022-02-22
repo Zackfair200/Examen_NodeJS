ef8ef151955528e59fee91d18466da40561d2e73
Alejandro Ponce <JandroPonPal@gmail.com>
2022-02-22 12:24:22 +0100

    add user in users files

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const pool = require("../db/database")
+const {encriptado} = require("../helpers/helpers")
 
-/* GET users listing. */
+/* LISTAR USUARIOS. */
 router.get("/", async (req, res, next) => {
   const usuarios = await pool.query("SELECT * FROM users");
   res.render("listusers", {
@@ -10,5 +11,33 @@ router.get("/", async (req, res, next) => {
     title: "Usuarios",
   });
 });
+
+/* AÑADIR USUARIOS */
+router.get("/add", (req, res, next) => {
+  res.render("adduser", {
+    title: "Añadir usuario",
+  });
+});
+
+router.post("/add", async (req, res, next) => {
+  const { fullname, username, email, password } = req.body;
+  const newUser = {
+    fullname,
+    username,
+    email,
+    password,
+  };
+  newUser.password = encriptado(newUser.password)
+  await pool.query("INSERT INTO users SET ?", [newUser]);
+  res.redirect("/users");
+});
+
+/* BORRAR USUARIOS */
+router.get("/delete/:id", async (req, res, next) => {
+  const {id} = req.params
+  await pool.query("DELETE FROM users WHERE id=?",[id])
+  res.redirect("/users");
+});
+
 
 module.exports = router;
