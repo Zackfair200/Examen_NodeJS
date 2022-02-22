fb9640100637059fd247064ea1709e57f20d32b0
Alejandro Ponce <JandroPonPal@gmail.com>
2022-02-22 13:08:42 +0100

    finished users & cars CRUD

---
+++ routes/cars.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var router = express.Router();
+const pool = require("../db/database");
+
+/* LISTAR COCHES. */
+router.get("/", async (req, res, next) => {
+  const coches = await pool.query("SELECT * FROM cars");
+  res.render("listcars", {
+    coches,
+    title: "Coches",
+  });
+});
+/* AÑADIR COCHES */
+router.get("/add", (req, res, next) => {
+  res.render("addcar", {
+    title: "Añadir coche",
+  });
+});
+
+router.post("/add", async (req, res, next) => {
+  const { modelo, numero, imagen } = req.body;
+  const newCar = {
+    modelo,
+    numero,
+    imagen,
+  };
+  await pool.query("INSERT INTO cars SET ?", [newCar]);
+  res.redirect("/cars");
+});
+/* BORRAR COCHES */
+router.get("/delete/:id", async (req, res, next) => {
+  const { id } = req.params;
+  await pool.query("DELETE FROM cars WHERE id=?", [id]);
+  res.redirect("/cars");
+});
+/* EDITAR COCHES */
+router.get("/editcar/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const edited = await pool.query("SELECT * FROM cars WHERE id=?", [id]);
+  res.render("editcar",edited[0]);
+});
+
+router.post("/editcar/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { modelo, numero, imagen } = req.body;
+  const editedCar = {
+    modelo,
+    numero,
+    imagen,
+  };
+  await pool.query("UPDATE cars SET ? WHERE id=?", [editedCar,id]);
+  res.redirect("/cars");
+});
+
+/* MOSTRAR JSON */
+
+
+module.exports = router;
